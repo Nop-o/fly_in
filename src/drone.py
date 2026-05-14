101eee12b9333c2081ac66b18d5cd2a6804d57a3
adamez-f <adamez-f@student.42lyon.fr>
2026-05-15 00:29:03 +0200

    fix: drone base

--- src/drone.py
+++ src/drone.py
@@ -1,12 +1,12 @@
 class Drone:
-    def __init__(self, color: str) -> None:
-        self.color = color
+    def __init__(self, id: int) -> None:
+        self.id = id
 
     @staticmethod
-    def drone_factory(nb_of_drone: int, color: str) -> list['Drone']:
+    def drone_factory(nb_of_drone: int) -> list['Drone']:
         created_drones: list[Drone] = []
         for i in range(nb_of_drone):
-            new_drone = Drone(color)
+            new_drone = Drone(i + 1)
             created_drones.append(new_drone)
         return created_drones
 
