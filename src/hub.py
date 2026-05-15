f0bb1e8b7c004e0b8b112cdb5463c60ec9d1eaf2
adamez-f <adamez-f@student.42lyon.fr>
2026-05-15 04:47:10 +0200

    fix: added zone_type enum and removed optional

--- src/hub.py
+++ src/hub.py
@@ -1,20 +1,48 @@
-from pydantic import BaseModel, Field, ValidationError
-from typing import Optional
+from pydantic import BaseModel, Field, ValidationError, model_validator
+from typing import Any
+from connection import Connection
+from zone_type import ZoneType
 
+class Hub(BaseModel):  
 
-class Hub(BaseModel):
     zone_name: str = Field(min_length=3, max_length=20)
     x: int = Field(ge=0)
     y: int = Field(ge=0)
-    zone: Optional[str] = Field(default="normal", min_length=6, max_length=10)
-    color: Optional[str] = Field(default=None, min_length=3, max_length=20)
-    max_drones: Optional[int] = Field(default=1, ge=0)
+    zone_type: ZoneType = Field(default=ZoneType.NORMAL)
+    color: str = Field(default=None, min_length=3, max_length=20)
+    max_drones: int = Field(default=1, ge=0)
+
+    @model_validator(mode='before')
+    @classmethod
+    def update_zone(cls, data: dict[str, Any]) -> dict[str, Any]:
+        if not data.get("zone_type"):
+            data["zone_type"] = ZoneType.NORMAL
+        elif data["zone_type"] == "normal":
+            data["zone_type"] = ZoneType.NORMAL
+        elif data["zone_type"] == "blocked":
+            data["zone_type"] = ZoneType.BLOCKED
+        elif data["zone_type"] == "restricted":
+            data["zone_type"] = ZoneType.RESTRICTED
+        elif data["zone_type"] == "priority":
+            data["zone_type"] = ZoneType.PRIORITY
+        else:
+            raise ValueError("Hub error: impossible zone type for "
+                                f"{data['zone_name']}")
+        return data
+    
+    def update_hub_connection(self, connections: list[Connection]) -> None:
+        self.connections = connections
 
 
 def main() -> None:
     try:
-        hub = Hub(hub_type="normal", zone_name="z1h1", x=0, y=0)
-        print(hub)
+        hub = Hub(zone_name="z1h1", x=1, y=0)
+        print(hub.x)
+        print(hub.y)
+        print(hub.zone_name)
+        print(hub.zone_type.value)
+        print(hub.color)
+        print(hub.max_drones)
     except ValidationError as e:
         print(e.errors()[0]["msg"].replace("Value error, ", ""))
     except Exception as e:
