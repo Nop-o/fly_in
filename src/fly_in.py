db8ad2641e619427c109d23d3a37a50afa80a522
arnaud_damez <arnaudnop@gmail.com>
2026-05-30 22:04:45 +0200

    secured pydantic import

--- src/fly_in.py
+++ src/fly_in.py
@@ -1,18 +1,26 @@
-from parsing import ValidateData
-from drone_map import DroneMap
-from pydantic import ValidationError
-
-
 def main() -> None:
+    try:
+        from pydantic import ValidationError
+    except ModuleNotFoundError as e:
+        print("Import error: pydantic is not installed, run the 'make install' command first")
+        return
+
+    from parsing import ValidateData
+    from drone_map import DroneMap
+
     try:
         file_content: ValidateData = ValidateData("test.txt")
         parsed_data = file_content.parse_file_content()
         drone_map = DroneMap(**parsed_data)
+        drone_map.create_drones()
+        print(drone_map.drones)
     except ValidationError as e:
         print(e.errors()[0]['msg'].replace("Value error, ", ""))
-    except Exception as e:
-        print(e)
+    
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(e)
