2c81a1fc97bec57b351ff45e32db1b02f64985cb
adamez-f <adamez-f@student.42lyon.fr>
2026-06-01 19:58:52 +0200

    minor changes

--- src/fly_in.py
+++ src/fly_in.py
@@ -1,23 +1,25 @@
+from parsing import ValidateData
+from drone_map import DroneMap
+
+
 def main() -> None:
     try:
         from pydantic import ValidationError
     except ModuleNotFoundError:
         print("Import error: pydantic is not installed, run the "
-              "'make install' command first")
+                "'make install' command first")
         return
-
-    from parsing import ValidateData
-    from drone_map import DroneMap
 
     try:
         file_content: ValidateData = ValidateData("src/test.txt")
         parsed_data = file_content.parse_file_content()
         drone_map = DroneMap(**parsed_data)
-        #drone_map.create_drones()
-        #for drone in drone_map.drones:
-        #    print(drone.position)
+        drone_map.create_drones()
     except ValidationError as e:
         print(e.errors()[0]['msg'].replace("Value error, ", ""))
+
+    drone_map.update_all_solution()
+    print(drone)
 
 
 if __name__ == "__main__":
