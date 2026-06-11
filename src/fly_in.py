9b668277c678ca4c382af58b7d57c8122b6de531
adamez-f <adamez-f@student.42lyon.fr>
2026-06-11 17:18:05 +0200

    restructured main

--- src/fly_in.py
+++ src/fly_in.py
@@ -3,13 +3,6 @@ import sys
 
 
 def main() -> None:
-    if len(sys.argv) == 1:
-        print("Argument error: no arguments provided")
-        return
-    if len(sys.argv) > 2:
-        print("Argument error: too many arguments provided")
-        return
-
     try:
         from pydantic import ValidationError
     except ModuleNotFoundError:
@@ -17,11 +10,19 @@ def main() -> None:
               "'make install' command first")
         return
 
-    from .drone_map import DroneMap
+    if len(sys.argv) == 1:
+        print("Argument error: no arguments provided")
+        return
+    if len(sys.argv) > 2:
+        print("Argument error: too many arguments provided")
+        return
+
 
     try:
         file_content: ValidateData = ValidateData(sys.argv[1])
         parsed_data = file_content.parse_file_content()
+
+        from .drone_map import DroneMap
 
         drone_map = DroneMap(**parsed_data)
         drone_map.create_drones()
